0edb91996c05057f45f71de5c280c2ede330d911
Gergő Boros <borosgergo@yahoo.com>
2024-12-05 13:45:50 +0100

    Update environment.ts

--- src/app/enviroment/environment.ts
+++ src/app/enviroment/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
   endpoint:
     'https://borosgergo00.cognitiveservices.azure.com/face/v1.0/detect',
   subscriptionKey:
-    'ad56a9cb639d436ca82c0fb2bc9cbbd9'
+    'expired'
 };
